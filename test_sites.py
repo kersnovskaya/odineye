d0d175f9259f70ce91b6b2b43f3a942ef5bf6dd2
Морозов Иван Юрьевич <morozov_i@bezlimit.ru>
2023-04-19 15:52:28 +0000

    test_sites.py edited online with Bitbucket

--- test_sites.py
+++ test_sites.py
@@ -1,8 +1,22 @@
 import requests
 import telebot
 
-bot = telebot.TeleBot('6076174980:AAGxe5VXrRpSvoEvbdrCPfmA3_By7TM0588')
+def send_to_telegram(text, token, chat):
+    url = "https://api.telegram.org/bot" + token + "/sendMessage"  # 1388568494:AAFZCASLFx64WZnpQLyqmBjht66Y3LU9xEI 5156460237:AAEt1if6meaEGae-8lVWp20Egj4TnBdDdEs
 
+    payload = {
+        "text": text,
+        "parse_mode": "Markdown",
+        "disable_web_page_preview": False,
+        "disable_notification": False,
+        "chat_id": chat
+    }
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
 
 def check_site(url):
     try:
@@ -30,4 +44,4 @@ sites = [
 for site in sites:
     if not check_site(site):
         # Отправляем сообщение на телеграм в случае ошибки
-        bot.send_message('-1001820930885', f'Ошибка на сайте {site}')
+        send_to_telegram(f'Ошибка на сайте {site}', '6076174980:AAGxe5VXrRpSvoEvbdrCPfmA3_By7TM0588', '-1001820930885')
